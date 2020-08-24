ca898e1d1a927b845b3112b65dcc6fd4a33d8857
nik-gus <niklas.gustafsson@gmail.com>
2020-08-25 00:34:44 +0200

    Test case added: TT3-2 AC-3 (adds recipe ingredients to shopping list)

--- cypress/integration/Handlingslista_spec.js
+++ cypress/integration/Handlingslista_spec.js
@@ -4,30 +4,33 @@ describe('Handlingslista', () => {
         cy.visit('/?dev')
     })
 
-    it('creates list - TT3-2 AC-1', () => {
+    it('creates shopping list - TT3-2 AC-1', () => {
        cy.createShoppingList('Test_list')
     })
 
-    it('adds ingredienses to List - TT3-2 AC-2', () => {
+    it('adds ingredienses to shopping list - TT3-2 AC-2', () => {
         cy.createShoppingList('Test_list')
         cy.addIngredientsToList('Test_list', ['Potatis', 'Banan', 'Aprikoser'])
     })
 
-        //Todo or not to do, thats the question
-    xit('adds recipe ingredients to list - TT3-2 AC-3', () => {
+    /** This test case verifies the existence of the created list, containing at least one item. 
+        It does NOT verify that the list contains the correct ingredients from the recipe. */
+    it('adds recipe ingredients to shopping list - TT3-2 AC-3', () => {
+        cy.verifyRecipe('sängkantsmeny', 'Smoothie med variation')
+        cy.addCurrentRecipeToNewShoppingList('Recipe_shopping_list')
     })
 
-    it('removes ingredintses from list - TT3-2 AC-4', () => {
+    it('removes ingredintses from shopping list - TT3-2 AC-4', () => {
         cy.createShoppingList('Test_list')
         cy.addIngredientsToList('Test_list', ['Potatis', 'Banan', 'Aprikoser'])
         cy.removeIngredient('Test_list', 'Banan')
     })
 
-        //Todo or not to do, thats the question
-    xit('sends list as email - TT3-2 AC-5', () => {
+        //Todo or not todo, thats the question
+    xit('sends shopping list as email - TT3-2 AC-5', () => {
     })
 
-    it('deletes list - TT3-2 AC-6', () => {
+    it('deletes shopping list - TT3-2 AC-6', () => {
         cy.createShoppingList('List1')
         cy.createShoppingList('List2')
         cy.createShoppingList('List3')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -74,35 +74,6 @@ Cypress.Commands.add('createShoppingList', (listName) => {
         .click() 
  })
 
-/*
-*************************************************************************************************
- */
-
-Cypress.Commands.add('deleteShoppingList', (listName) => {
-    //Navigate to /shopping_list 
-    cy
-        .contains('Handlingslista', { timeout: 10000 })
-        .click()
-
-    // DOM navigation in order to select 'Ta bort' on this specific list
-    cy
-        .contains(listName)
-        .parent()
-        .next()
-        .children() // onödig?
-        .should('have.text', 'Ta bort')
-        .click()
-
-    // Assert delete
-    cy
-        .contains(listName)
-        .should('not.exist')
-
-    //Go back to start page
-    cy
-        .get('.rn-1habvwh > .rn-1loqt21 > .rn-11yh6sk > .rn-1272l3b')
-        .click() 
-}) 
 
 /*
 *************************************************************************************************
@@ -173,6 +144,43 @@ Cypress.Commands.add('addIngredientsToList', (listName, ingredientsArr) => {
 *************************************************************************************************
  */
 
+Cypress.Commands.add('addCurrentRecipeToNewShoppingList', (listName) => {
+    // Select shopping cart
+    cy
+        .get(':nth-child(2) > :nth-child(1) > .rn-11yh6sk > .rn-1272l3b')
+        .click()
+    
+    // Select Lägg till
+    cy
+        .get('.rn-16y2uox > .rn-4xf99a > .rn-13yce4e')
+        .click()
+
+    // Name new list {listName}
+    cy
+        .get('.rn-97bpaa')
+        .type(listName)
+
+    // Select Skapa   ---  (skakig?... hur använda contains på andra 'Skaka'-förekomsten?) 
+    cy
+        .get('.rn-14lw9ot > :nth-child(2) > .rn-1awozwy > .rn-13yce4e')
+        .click()
+
+    //Go back to start page
+    cy
+    .get('.rn-1habvwh > .rn-1loqt21 > .rn-11yh6sk > .rn-1272l3b').click() 
+    .get('.rn-1habvwh > .rn-1loqt21 > .rn-11yh6sk > .rn-1272l3b').click() 
+    .get('.rn-1habvwh > .rn-1loqt21 > .rn-11yh6sk > .rn-1272l3b').click() 
+
+    // Assert that newly created list extist, containing at least one item
+    cy.contains('Handlingslista').click()
+    cy.contains(listName).click()
+    cy.contains('Ta bort')
+})
+
+/*
+*************************************************************************************************
+ */
+
 Cypress.Commands.add('removeIngredient', (listName, ingridient) => {
     // Navigate to /shopping_list 
     cy  
@@ -213,5 +221,36 @@ Cypress.Commands.add('removeIngredient', (listName, ingridient) => {
         .get('.rn-fzspga > .rn-13yce4e')
         .should('have.text', 'Matfröjd Start')
 })
+
+/*
+*************************************************************************************************
+ */
+
+Cypress.Commands.add('deleteShoppingList', (listName) => {
+    //Navigate to /shopping_list 
+    cy
+        .contains('Handlingslista', { timeout: 10000 })
+        .click()
+
+    // DOM navigation in order to select 'Ta bort' on this specific list
+    cy
+        .contains(listName)
+        .parent()
+        .next()
+        .children() // onödig?
+        .should('have.text', 'Ta bort')
+        .click()
+
+    // Assert delete
+    cy
+        .contains(listName)
+        .should('not.exist')
+
+    //Go back to start page
+    cy
+        .get('.rn-1habvwh > .rn-1loqt21 > .rn-11yh6sk > .rn-1272l3b')
+        .click() 
+}) 
+
 
 
