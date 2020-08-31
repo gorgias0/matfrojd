8c594ec70930dfc4d8cb3e3481a4aa6d0d438426
nik-gus <niklas.gustafsson@gmail.com>
2020-09-01 00:24:18 +0200

    Laga_mat_spec.js RC1

--- cypress/integration/Laga_mat_spec.js
+++ cypress/integration/Laga_mat_spec.js
@@ -1,24 +1,47 @@
 describe('Laga mat', () => {
 
+    const cookRecipe = (category, recipe) => {
+        cy.verifyRecipe(category, recipe)
+         
+         // Select Laga mat
+        cy
+            .contains('Laga mat')
+            .click()
+        
+            // Check all the cooking step checkboxes    
+        cy
+            .get('.rn-1x0uki6 > :nth-child(2) > :nth-child(1) > :nth-child(1) > .rn-1awozwy')
+            .parent()
+            .parent()
+            .click( { multiple: true} )
+            .nextAll()
+            .click({ multiple: true })
+        
+        // Assert greeting
+        cy  
+            .contains('Smaklig måltid')
+    
+        //back to startpage?? why?
+    }
 
     beforeEach(() =>  {
         cy.visit('/?dev')
     })
 
     it('cooks Kardemummakryddad mjölkdrink', () => {
-        cy.cookRecipe('sängkantsmeny', 'Kardemummakryddad mjölkdrink')
+        cookRecipe('sängkantsmeny', 'Kardemummakryddad mjölkdrink')
     })
 
     it('cooks Fisk i dillsås', () => {
-        cy.cookRecipe('lunch/Middag', 'Fisk i dillsås')
+        cookRecipe('lunch/Middag', 'Fisk i dillsås')
     })
 
     it('cooks Inlagd pressgurka', () => {
-        cy.cookRecipe('aptitretare', 'Inlagd pressgurka')
+        cookRecipe('drycker', 'Varm äppeldryck')
     })
 
-    it.only('cooks Fruktkompott', () => {
-        cy.cookRecipe('efterrätter', 'Fruktkompott')
+    it('cooks Fruktkompott', () => {
+        cookRecipe('efterrätter', 'Fruktkompott')
     })
 
 /*
@@ -30,8 +53,10 @@ describe('Laga mat', () => {
         cy.cookRecipe('', '')
     })
 
-    it('', () => {
+     it('', () => {
         cy.cookRecipe('', '')
-    }) */
+    })
+
+*/
 
 })
